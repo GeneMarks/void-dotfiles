dab4f7a2154bd9d67da56de16814f5f7b1924bf7
Gene Marks <genemarks74@gmail.com>
2024-06-19 01:55:14 -0400

    formatting fixes

--- .config/hypr/scripts/set_brightness.sh
+++ .config/hypr/scripts/set_brightness.sh
@@ -17,16 +17,15 @@ case $1 in
     up)
         if [ $BRIGHTNESS -lt 100 ]; then
             ddcutil setvcp 10 + 5
-	    BRIGHTNESS=$(($BRIGHTNESS + 5))
+            BRIGHTNESS=$(($BRIGHTNESS + 5))
         fi
         ;;
     down)
         if [ $BRIGHTNESS -gt 0 ]; then
             ddcutil setvcp 10 - 5
-	    BRIGHTNESS=$(($BRIGHTNESS - 5))
+            BRIGHTNESS=$(($BRIGHTNESS - 5))
         fi
         ;;
 esac
 
 notify-send "Brightness Control" "Brightness: ${BRIGHTNESS}%" -h int:value:$BRIGHTNESS -h string:x-canonical-private-synchronous:sys-notify -t 3000
-
