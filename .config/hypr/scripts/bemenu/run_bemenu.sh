3d368510871bb418e4ef69c855d5af0ca8e02cc7
Gene Marks <genemarks74@gmail.com>
2024-06-19 20:35:07 -0400

    fix: put bemenu params directly in command

--- .config/hypr/scripts/bemenu/run_bemenu.sh
+++ .config/hypr/scripts/bemenu/run_bemenu.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-source $MY_HYPR_SCRIPTS/bemenu/bemenu_default_params.sh
-
-bemenu ${BEMENU_DEFAULT_PARAMS[@]} $@
+bemenu \
+    --single-instance \
+    --ignorecase \
+    --center \
+    --list 10 \
+    --fixed-height \
+    --width-factor 0.26 \
+    --line-height 32 \
+    --no-spacing \
+    --scrollbar autohide \
+    --prompt 'bemenu' \
+    --fn 'SystemUI 10' \
+    --fb '#0f1014' \
+    --nf '#c7bcb7' \
+    --nb '#0f1014' \
+    --ab '#0f1014' \
+    --tb '#0f1014' \
+    --tf '#ffffff' \
+    --hb '#1b1d24' \
+    --hf '#c7bcb7' \
+    --scb '#0f1014' \
+    --scf '#5d6066' \
+    $@
