97781a1b00994e6cee7073b04b09d770096a67b5
Gene Marks <genemarks74@gmail.com>
2024-06-20 23:46:18 -0400

    uncomment auto_session_allowed_dirs

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -1,10 +1,9 @@
 return {
     "rmagatti/auto-session",
     opts = {
-        -- globbing broken
-        -- auto_session_allowed_dirs = {
-        --     "~/.dotfiles/*",
-        -- },
+        auto_session_allowed_dirs = {
+            "~/.dotfiles/*",
+        },
         bypass_session_save_file_types = {
             "fugitive",
         },
