c7c61161b42c8b98a0d136d61576dd618872e77e
Gene Marks <genemarks74@gmail.com>
2024-06-25 21:35:29 -0400

    implement audio cover art previews

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -13,15 +13,25 @@ generate_video_frame() {
     ffmpeg -y -ss "$FRAME_TIME" -i "$1" -vframes 1 "$2"
 }
 
+save_cover_art() {
+    ffmpeg -y -i "$1" -an -c:v copy "$2"
+}
+
 case "$1" in
     *.png|*.jpg|*.jpeg|*.gif|*.bmp|*.tiff)
         run_chafa "$1" "$2" "$3"
         ;;
 
     *.mkv|*.mp4|*.avi|*.mov)
-        VIDEO_THUMB=$CACHE_DIR/video_thumb.jpg
-        generate_video_frame "$1" "$VIDEO_THUMB"
-        run_chafa "$VIDEO_THUMB" "$2" "$3"
+        VIDEO_THUMB_CACHE=$CACHE_DIR/video_thumb.jpg
+        generate_video_frame "$1" "$VIDEO_THUMB_CACHE"
+        run_chafa "$VIDEO_THUMB_CACHE" "$2" "$3"
+        ;;
+
+    *.mp3|*.flac|*.m4a)
+        ALBUM_ART_CACHE=$CACHE_DIR/album_art.jpg
+        save_cover_art "$1" "$ALBUM_ART_CACHE"
+        run_chafa "$ALBUM_ART_CACHE" "$(echo "$2 / 2" | bc)" "$(echo "$3 / 2" | bc)"
         ;;
 
     *)
