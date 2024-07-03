4a37083892218666ceb9e6fdbb582e9d21224955
Gene Marks <genemarks74@gmail.com>
2024-07-02 23:33:40 -0400

    add ~/.local/bin to $PATH

--- .config/zsh/env_variables.zsh
+++ .config/zsh/env_variables.zsh
@@ -1,10 +1,10 @@
 ### Environment variables ###
 
-# General
+export PATH=$HOME/.local/bin:$PATH
+
 export SCRIPTS=$HOME/.scripts
 export EDITOR=nvim
 
-# History settings
 export HISTSIZE=10000
 export SAVEHIST=10000
 export HISTFILE=$HOME/.zsh_history
