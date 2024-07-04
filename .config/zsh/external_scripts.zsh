c396baf85621f3d0ecf12dc9a328660211b353ff
Gene Marks <genemarks74@gmail.com>
2024-07-04 00:41:14 -0400

    add zsh-autopair plugin

--- .config/zsh/external_scripts.zsh
+++ .config/zsh/external_scripts.zsh
@@ -1,5 +1,14 @@
 ### External scripts ###
 
+# autopair
+if [[ ! -d $HOME/.zsh-autopair ]]; then
+    git clone https://github.com/hlissner/zsh-autopair ~/.zsh-autopair
+fi
+
+source $HOME/.zsh-autopair/autopair.zsh
+autopair-init
+
+# nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
