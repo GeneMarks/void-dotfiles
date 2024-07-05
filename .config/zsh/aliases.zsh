0a8f686551dc93d64ed4e7ea8ae0c6ebdc06ca31
Gene Marks <genemarks74@gmail.com>
2024-07-05 12:07:50 -0400

    add reboot and shutdown aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,5 +1,10 @@
 ### Aliases ###
 
+# Void has bug where shutdown/reboot hang when network
+# shares are mounted.
+alias sd='sudo umount -a -r -t nfs,nfs4,cifs && loginctl poweroff'
+alias rb='sudo umount -a -r -t nfs,nfs4,cifs && loginctl reboot'
+
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 
