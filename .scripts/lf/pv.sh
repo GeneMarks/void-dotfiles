b53c5ddca79d2227afef8ac62cf1e9b56a3c2a34
Gene Marks <genemarks74@gmail.com>
2024-07-12 15:55:39 -0400

    add gif preview support

--- .scripts/lf/pv.sh
+++ .scripts/lf/pv.sh
@@ -10,21 +10,26 @@ print_image() {
 generate_video_frame() {
     DURATION=$(ffprobe -i "$1" -show_entries format=duration -v quiet -of csv="p=0")
     FRAME_TIME=$(echo "$DURATION * 0.05" | bc)
-    ffmpeg -y -ss "$FRAME_TIME" -i "$1" -vframes 1 "$2"
+    ffmpeg -y -ss "$FRAME_TIME" -i "$1" -vframes 1 "$2" || rm -f "$2"
+}
+
+extract_gif_frame() {
+    magick "$1[0]" $2 || rm -f "$2"
 }
 
 extract_cover_art() {
-    ffmpeg -y -i "$1" -an -c:v copy "$2" &> /dev/null
-
-    # Delete old cache if extraction failed
-    if [[ $? -ne 0 ]]; then
-        rm -f "$ALBUM_ART_CACHE"
-    fi
+    ffmpeg -y -i "$1" -an -c:v copy "$2" &> /dev/null || rm -f "$2"
 }
 
 case "$1" in
-    *.png|*.jpg|*.jpeg|*.gif|*.bmp|*.tiff|*.webp)
+    *.png|*.jpg|*.jpeg|*.bmp|*.tiff|*.webp)
         print_image "$1" "$2" "$3"
+        ;;
+
+    *.gif)
+        GIF_FRAME_CACHE=$CACHE_DIR/gif_frame.jpg
+        extract_gif_frame "$1" "$GIF_FRAME_CACHE"
+        print_image "$GIF_FRAME_CACHE" "$2" "$3"
         ;;
 
     *.mkv|*.mp4|*.avi|*.mov)
