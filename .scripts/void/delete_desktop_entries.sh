6990a25f717f7c24b12e8e726e557f73a859a770
Gene Marks <genemarks74@gmail.com>
2025-02-06 23:51:36 -0500

    add void scripts folder

---
+++ .scripts/void/delete_desktop_entries.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+DESKTOP_DIR="/usr/share/applications"
+
+rm -f "$DESKTOP_DIR/chromium.desktop"
+rm -f "$DESKTOP_DIR/firefox.desktop"
+rm -f "$DESKTOP_DIR/foot.desktop"
+rm -f "$DESKTOP_DIR/footclient.desktop"
+rm -f "$DESKTOP_DIR/foot-server.desktop"
+rm -f "$DESKTOP_DIR/htop.desktop"
+rm -f "$DESKTOP_DIR/lf.desktop"
+rm -f "$DESKTOP_DIR/mpv.desktop"
+rm -f "$DESKTOP_DIR/nvim.desktop"
+rm -f "$DESKTOP_DIR/org.kde.dolphin.desktop"
+rm -f "$DESKTOP_DIR/org.pulseaudio.pavucontrol.desktop"
+rm -f "$DESKTOP_DIR/spek.desktop"
+rm -f "$DESKTOP_DIR/thunderbird.desktop"
+rm -f "$DESKTOP_DIR/vlc.desktop"
