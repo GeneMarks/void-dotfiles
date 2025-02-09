b25d67c526c8918995064fef9e11303ced5d1ba2
Gene Marks <genemarks74@gmail.com>
2025-02-09 17:11:39 -0500

    show hidden files in oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -3,6 +3,9 @@ return {
     config = function()
         require("oil").setup({
             delete_to_trash = true,
+            view_options = {
+                show_hidden = true,
+            },
         })
 
         vim.keymap.set("n", "<leader>o", "<CMD>Oil<CR>", { desc = "Oil" })
