d56ced5fed55160255b4523ce3537165cca0b60d
Gene Marks <genemarks74@gmail.com>
2025-02-28 12:47:13 -0500

    add alias to boot into windows 10

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -4,6 +4,7 @@
 # shares are mounted.
 alias shutdown="sudo umount -a -r -t nfs,nfs4,cifs && loginctl poweroff"
 alias reboot="sudo umount -a -r -t nfs,nfs4,cifs && loginctl reboot"
+alias win10="sudo efibootmgr --bootnext 0000 && reboot"
 
 alias ls="ls --color=auto"
 alias grep="grep --color=auto"
