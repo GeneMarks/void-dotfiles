579a20a6c142f7c1c63a9dfc4bfc0c74ac4d9fc7
Gene Marks <genemarks74@gmail.com>
2025-04-25 11:36:59 -0400

    override cursor bg

--- .config/nvim/lua/colorschemes/mellifluous.lua
+++ .config/nvim/lua/colorschemes/mellifluous.lua
@@ -2,5 +2,16 @@ return {
     "ramojus/mellifluous.nvim",
     name = "mellifluous",
     lazy = false,
-    priority = 1000
+    priority = 1000,
+    config = function()
+        require("mellifluous").setup({
+            highlight_overrides = {
+                light = function(hl, colors)
+                    hl.set("Cursor", { bg = "#b8b8b8", fg = colors.bg })
+                    hl.set("lCursor", { link = "Cursor" })
+                    hl.set("CursorIM", { link = "Cursor" })
+                end,
+            }
+        })
+    end
 }
