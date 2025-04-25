e19788eb64f221fcbacaaba26f57fb19fefffa51
Gene Marks <genemarks74@gmail.com>
2025-04-25 10:36:31 -0400

    formatting

--- .config/zsh/env_variables.zsh
+++ .config/zsh/env_variables.zsh
@@ -5,9 +5,10 @@ export PATH="$PATH:$HOME/.cargo/bin"
 export DOTNET_ROOT"=$HOME/.dotnet"
 export PATH="$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools"
 
-export SCRIPTS="$HOME/.scripts"
-export EDITOR="nvim"
-
 export HISTSIZE="10000"
 export SAVEHIST="10000"
 export HISTFILE="$HOME/.zsh_history"
+
+export SCRIPTS="$HOME/.scripts"
+export EDITOR="nvim"
+export BROWSER="firefox"
